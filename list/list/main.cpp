f207857bcee77f515dc4c70a7b80bbed2bf71115
leesj <tkdrmsdl80@naver.com>
2022-06-14 12:50:08 +0900

    쉅 0614

--- list/list/main.cpp
+++ list/list/main.cpp
@@ -1,4 +1,4 @@
-//0.1 
+//0.2
 
 #include <iostream>
 
@@ -8,17 +8,18 @@ using namespace std;
 
 struct List
 {
+	List* Front;
 	int Value;
 	List* Back;
 };
 
-void AddObject(List* _Next, const int& _Value);
 
-void Output(List* _Next);
+void push_back(List* _Current, const int& _Value);
+void insert(List* _Current, const int _Where, const int& _Value);
+void erase(List* _Current, const int _Where);
 
-//숙제 인설트 함수 만들기 하면 지우개만들기
+void Output(const List* _Current);
 
-void Instert();
 
 int main(void)
 {
@@ -27,41 +28,82 @@ int main(void)
 	NumberList->Value = 0;
 	NumberList->Back = nullptr;
 
+
 	for (int i = 0; i < 10; ++i)
-		AddObject(NumberList, 1 * 10 + 10);
+		push_back(NumberList, i * 10 + 10);
+
+
+	insert(NumberList, 2, 15);
 	Output(NumberList);
 
 
-	//cout << NumberList->Value << endl;
-	//cout << NumberList->Back->Value << endl;
-	//cout << NumberList->Back->Back->Value << endl;
-	
-
+	erase(NumberList, 2);
+	Output(NumberList);
 
 
 	return 0;
 }
 
-void AddObject(List* _Next, const int& _Value)
+void push_back(List* _Current, const int& _Value)
 {
-	if (_Next->Back == nullptr)
+	if (_Current->Back == nullptr)
 	{
 		List* Temp = new List;
-		Temp->Value = _Value;
+		
+		//뭔가 비어잇음
+
 		Temp->Back = nullptr;
 
-		_Next->Back = Temp;
-
+		_Current->Back = Temp;
 	}
 	else
-		AddObject(_Next->Back, _Value);
+		push_back(_Current->Back, _Value);
 }
 
-
-void Output(List* _Next)
+void insert(List* _Current, const int _Where, const int& _Value)
 {
-	cout << _Next->Value << endl;
-	if (_Next->Back != nullptr)
-		Output(_Next->Back);
+	if (_Where == 1)
+	{
+		List* Temp = new List;
+
+		Temp->Value = _Value;
+
+		Temp->Back = _Current->Back;
+		_Current->Back = Temp;
+	}
+	else
+		insert(_Current->Back, _Where - 1, _Value);
 }
 
+
+List* Front = nullptr;
+
+void erase(List* _Current, const int _Where)
+{
+	if (_Where == 0)
+	{
+		//_Current->Front;
+		//Temp->Front = _Current;
+
+		
+
+		delete _Current;
+		_Current = nullptr;
+	}
+	else
+	{
+		Front = _Current;
+		erase(_Current->Back, _Where - 1);
+	}
+}
+
+void Output(const List* _Current)
+{
+	cout << _Current->Value << endl;
+
+}
+
+/*
+
+
+*/
